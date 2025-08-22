da7a03013a44428517b038574488f841cc4ec66f
Angie Samson <97255743+AnzuWanzu@users.noreply.github.com>
2025-08-22 18:56:05 +0800

    added sample endpoints for post, put, and delete. Added nodemon for seamless api update

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,19 @@ const app = express();
 const PORT = 5001;
 
 app.get("/api/notes", (req, res) => {
-  res.send("You got 5 notes");
+  res.status(200).send("You got 10 notes");
+});
+
+app.post("/api/notes", (req, res) => {
+  res.status(201).json({ message: "Note created successfully!" });
+});
+
+app.put("/api/notes/:id", (req, res) => {
+  res.status(200).json({ message: "Note updated successfully!" });
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+  res.status(200).json({ message: "Note deleted successfully!" });
 });
 
 app.listen(PORT, () => {
