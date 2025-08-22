7b22b0d089504483225e3af04b384e6b90c9c28c
Angie Samson <97255743+AnzuWanzu@users.noreply.github.com>
2025-08-22 23:09:53 +0800

    Added getNoteById function, getAllNotes sorts latest note.

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -1,11 +1,24 @@
 import Note from "../models/Note.js";
 
-export const getAllNotes = async (req, res) => {
+export const getAllNotes = async (_, res) => {
   try {
-    const notes = await Note.find();
+    const notes = await Note.find().sort({ createdAt: -1 });
     res.status(200).json(notes);
   } catch (error) {
     console.log("Error in getAllNotes controller", error);
+
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const getNoteById = async (req, res) => {
+  try {
+    const note = await Note.findById(req.params.id);
+    if (!note) return res.status(404).json({ message: "Note not found" });
+    res.json(note);
+    res.status(200).json(note);
+  } catch (error) {
+    console.log("Error in getNoteById controller", error);
 
     res.status(500).json({ message: "Internal server error" });
   }
--- backend/src/routes/notesRoutes.js
+++ backend/src/routes/notesRoutes.js
@@ -4,11 +4,14 @@ import {
   deleteNote,
   updateNote,
   getAllNotes,
+  getNoteById,
 } from "../controllers/notesController.js";
 
 const router = express.Router();
 
 router.get("/", getAllNotes);
+
+router.get("/:id", getNoteById);
 
 router.post("/", createNote);
 
