83953f4e290630f2b1f04d80afe44f50f7405ece
Angie Samson <97255743+AnzuWanzu@users.noreply.github.com>
2025-08-24 00:21:49 +0800

    Added logic for serve react app for production. Npm run start can now be used onto root folder

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
@@ -10,16 +11,19 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5002;
+const __dirname = path.resolve();
 
 //----Middleware---
 
 //this allows us to use functions such as const { title, content } = req.body; or
 //parse JSON bodies: req.body
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  })
-);
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+}
 
 app.use(express.json());
 app.use(rateLimiter);
@@ -33,6 +37,13 @@ app.use(rateLimiter);
 //--------
 
 app.use("/api/notes", notesRoutes); //best to use when controllers have similar routes
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
